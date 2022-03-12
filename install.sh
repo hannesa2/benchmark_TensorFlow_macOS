cf8b5da55e9a18e7f8429c847d1302cb895f0982
Hannes Achleitner <hannes.software@gmx.at>
2022-03-12 18:33:22 +0100

    CI

--- install.sh
+++ install.sh
@@ -1,18 +1,17 @@
-mkdir Downloads
+mkdir Downloads || echo "Already exists"
 cd Downloads
-wget https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-MacOSX-arm64.sh
+curl -fsSLo Miniforge3.sh "https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-MacOSX-$(uname -m).sh"
+bash Miniforge3.sh -b
 cd ..
-chmod +x ./Downloads/Miniforge3-MacOSX-arm64.sh
-sh ./Downloads/Miniforge3-MacOSX-arm64.sh
 source ~/miniforge3/bin/activate
-python -m pip uninstall tensorflow-macos
-python -m pip uninstall tensorflow-metal
+#yes | python -m pip uninstall tensorflow-macos
+#yes | python -m pip uninstall tensorflow-metal
 
-conda install -c apple tensorflow-deps
-python -m pip uninstall tensorflow-macos
-python -m pip uninstall tensorflow-metal
-conda install -c apple tensorflow-deps --force-reinstall
-conda install -c apple tensorflow-deps==2.6.0
-python -m pip install tensorflow-macos # <-- new
-python -m pip install tensorflow-metal
+yes | conda install -c apple tensorflow-deps
+yes | python -m pip uninstall tensorflow-macos
+yes | python -m pip uninstall tensorflow-metal
+yes | conda install -c apple tensorflow-deps --force-reinstall
+yes | conda install -c apple tensorflow-deps==2.6.0
+yes | python -m pip install tensorflow-macos # <-- new
+yes | python -m pip install tensorflow-metal
 ./run_BENCHMARK.sh
